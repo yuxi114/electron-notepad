03f92e3034ace156c20adb2fc47adac6c9a83141
yuxi114 <yuxi1@jd.com>
2019-04-22 15:41:01 +0800

    push

---
+++ src/appmenu.js
@@ -0,0 +1,45 @@
+const { app } = require('electron');
+
+export const appMenuTemplate = [
+    {
+        label: 'File',
+        submenu: []
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+        ]
+    },
+    {
+        role: 'help',
+        submenu: [
+            {
+                label: 'Home Page',
+                click() { require('electron').shell.openExternal('https://github.com/yuxi114'); }
+            }
+        ]
+    }
+];
